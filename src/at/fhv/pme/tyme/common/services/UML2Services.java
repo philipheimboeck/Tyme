0601139f947adcaae99df9517acd3ae5d967b303
Philip Heimböck <philipheimboeck@gmail.com>
2015-11-12 12:10:18 +0100

    NOT WORKING: Trying to generate Java Types

--- src/at/fhv/pme/tyme/common/services/UML2Services.java
+++ src/at/fhv/pme/tyme/common/services/UML2Services.java
@@ -1,8 +1,16 @@
 package at.fhv.pme.tyme.common.services;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Scanner;
 
 import org.eclipse.uml2.uml.Element;
+import org.eclipse.uml2.uml.Namespace;
+import org.eclipse.uml2.uml.Operation;
+import org.eclipse.uml2.uml.Parameter;
+import org.eclipse.uml2.uml.ParameterDirectionKind;
 import org.eclipse.uml2.uml.Stereotype;
 
 public class UML2Services {
@@ -21,5 +29,57 @@ public class UML2Services {
 			}
 		}
 		return false;
+	}
+	
+
+	/**
+	 * Get the package name as file path
+	 * @param pakkage The package to convert
+	 * @return The package name as file path
+	 */
+	public String toPath(Namespace pakkage) {
+		return pakkage.getName().replace(".", File.separator) + File.separator;
+	}
+
+	/**
+	 * Convert a multine string correctly to a java multine line string
+	 * @param multiline The multiline string
+	 * @return The converted string
+	 */
+	public String convertToMultiLineString(String multiline) {
+		StringBuilder builder = new StringBuilder();
+		try (Scanner scanner = new Scanner(multiline)) {
+			while(scanner.hasNextLine()){
+				builder.append("\"").append(scanner.nextLine()).append("\"+\n");
+			}
+		}
+		
+		// remove last line break
+		builder.setLength(builder.length() - "\"+\n".length());
+		
+		// remove first "
+		String substring =  builder.substring(1);
+		
+		// escape \
+		String escaped = substring.replace("\\", "\\\\");
+		
+		return escaped;
+	}
+	
+	/**
+	 * Return a list of parameters of an operation without the return type
+	 * @param node
+	 * @return
+	 * @throws FactoryException
+	 */
+	public List<Parameter> getRealParameters(Operation operator) {
+		List<Parameter> parListFiltered = new ArrayList<Parameter>();
+		List<Parameter> parList = operator.getOwnedParameters();
+		for (Iterator<Parameter> iterPar = parList.iterator(); iterPar.hasNext();) {
+			Parameter par = (Parameter) iterPar.next();
+			if (!par.getDirection().equals(ParameterDirectionKind.RETURN_LITERAL))
+				parListFiltered.add(par);
+		}
+		return parListFiltered;		
 	}
 }
